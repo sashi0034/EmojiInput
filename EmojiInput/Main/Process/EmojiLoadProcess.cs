f55ef374e1912d843823907492444d966702f9a3
sashi0034 <satoshi3104.nishi@gmail.com>
2024-01-01 19:33:01 +0900

    使用する絵文字画像を変更 🧞

--- EmojiInput/Main/Process/EmojiLoadProcess.cs
+++ EmojiInput/Main/Process/EmojiLoadProcess.cs
@@ -31,7 +31,7 @@ public record EmojiLoadProcess(
             {
                 var bitmap = new BitmapImage();
                 bitmap.BeginInit();
-                var iconPath = "Resource/emoji_icon/72/" + emoji.ImageFilename;
+                var iconPath = $"Resource/emoji_icon/aliased/{emoji.Aliases[0]}.png";
                 bitmap.UriSource = new Uri(iconPath, UriKind.Relative);
                 bitmap.CacheOption = BitmapCacheOption.OnLoad;
                 bitmap.EndInit();
