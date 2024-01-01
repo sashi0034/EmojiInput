e65e7824aba7b587724b395ad6b21f4549c189c2
sashi0034 <satoshi3104.nishi@gmail.com>
2024-01-01 18:32:43 +0900

    スキン絵文字対応の修正など 👩🏼‍🤝‍👨🏽

--- EmojiGenerator/Main.cpp
+++ EmojiGenerator/Main.cpp
@@ -1,7 +1,14 @@
-﻿# include <Siv3D.hpp> // Siv3D v0.6.12
+﻿#include <iso646.h>
+# include <Siv3D.hpp> // Siv3D v0.6.12
 
 namespace
 {
+	constexpr bool isSaveImage = false;
+	constexpr int commonImageSize = 64;
+	constexpr auto solutionRoot = U"../../"_sv;
+	constexpr auto skinOutputDirectory = U"../../EmojiInput/Resource/"_sv;
+	constexpr auto imageOutputDirectory = U"../../EmojiInput/Resource/emoji_icon/aliased/"_sv;
+
 	struct EmojiData
 	{
 		String emoji;
@@ -9,34 +16,62 @@ namespace
 		bool skin;
 	};
 
-	constexpr bool isSaveImage = true;
-	constexpr int commonImageSize = 64;
-	constexpr auto solutionRoot = U"../../"_sv;
-	constexpr auto outputDirectory = U"../../EmojiInput/Resource/emoji_icon/aliased/"_sv;
-}
-
-void saveEmojiImage(StringView outputDirectory, const String& filename, const String& emoji)
-{
-	auto image = Image(Emoji(emoji));
-	image.scale(((SizeF(image.size()) / image.size().maxComponent()) * commonImageSize).asPoint());;
-
-	if (image.size().minComponent() == 0)
+	void saveEmojiImage(StringView outputDirectory, const String& filename, const String& emoji)
 	{
-		Console.writeln(U"Invalid image: " + filename);
-		return;
+		auto image = Image(Emoji(emoji));
+		image.scale(((SizeF(image.size()) / image.size().maxComponent()) * commonImageSize).asPoint());;
+
+		if (image.size().minComponent() == 0)
+		{
+			Console.writeln(U"Invalid image: " + filename);
+			return;
+		}
+
+		if constexpr (not isSaveImage) return;
+
+		image.resize(Size::One() * commonImageSize);
+
+		if (image.save(outputDirectory + filename))
+		{
+			// Console.writeln(U"Saved: " + filename);
+		}
+		else
+		{
+			Console.writeln(U"Failed to save: " + filename);
+		}
 	}
 
-	if constexpr (not isSaveImage) return;
-
-	image.resize(Size::One() * commonImageSize);
-
-	if (image.save(outputDirectory + filename))
+	String getSkinEmoji(const EmojiData& e, char32_t skin)
 	{
-		// Console.writeln(U"Saved: " + filename);
+		String skinEmoji;
+		constexpr char32_t zwj = 0x200d;
+		const auto zwjIndex1 = e.emoji.indexOf(zwj);
+		const auto zwjIndex2 = e.emoji.indexOf(zwj, zwjIndex1 + 1);
+		if (zwjIndex1 == String::npos)
+		{
+			skinEmoji = e.emoji + skin;
+		}
+		else
+		{
+			skinEmoji = String(e.emoji).insert(zwjIndex1, String().append(skin));
+			if (zwjIndex2 != String::npos)
+			{
+				// 👩🏼‍🤝‍👨🏽 など2色の肌色
+				skinEmoji += skin;
+			}
+		}
+		return skinEmoji;
 	}
-	else
+
+	String getSkinMap(const StringView skinColors)
 	{
-		Console.writeln(U"Failed to save: " + filename);
+		String skinMetadata{};
+		for (auto&& skin : skinColors)
+		{
+			if (not skinMetadata.empty()) skinMetadata += U" ";
+			skinMetadata += U"{:X}"_fmt(static_cast<uint32>(skin));
+		}
+		return skinMetadata;
 	}
 }
 
@@ -70,29 +105,43 @@ void Main()
 		return;
 	}
 
+	Array<JSON> skinJsonArray{};
+	constexpr StringView skinColors = U"🏻🏼🏽🏾🏿";
+
 	// 絵文字を画像にして保存していく
 	for (int i = 0; i < emojis.size(); ++i)
 	{
 		const auto& e = emojis[i];
+		JSON jsonElement{};
 		if (e.skin)
 		{
-			constexpr StringView skinColors = U"🏻🏼🏽🏾🏿";
-			for (const auto skin : skinColors)
+			String concatenatedSkinEmoji{};
+			for (auto&& skin : skinColors)
 			{
 				const auto filename = e.aliases + U"_{:X}"_fmt(static_cast<uint32>(skin)) + U".png";
-				constexpr char32_t zwj = 0x200d;
-				const int zwjIndex = e.emoji.indexOf(zwj);
-				const auto skinEmoji = zwjIndex == e.emoji.npos
-					                       ? e.emoji + skin
-					                       : String(e.emoji).insert(zwjIndex, String().append(skin));
-				saveEmojiImage(outputDirectory, filename, skinEmoji);
+				String skinEmoji = getSkinEmoji(e, skin);
+				if (not concatenatedSkinEmoji.empty()) concatenatedSkinEmoji += U" "_sv;
+				concatenatedSkinEmoji += skinEmoji;
+				saveEmojiImage(imageOutputDirectory, filename, skinEmoji);
 			}
+			jsonElement[U"key"] = e.aliases;
+			jsonElement[U"emojis"] = concatenatedSkinEmoji;
+			skinJsonArray.push_back(jsonElement);
 		}
 		else
 		{
-			saveEmojiImage(outputDirectory, e.aliases + U".png", e.emoji);
+			saveEmojiImage(imageOutputDirectory, e.aliases + U".png", e.emoji);
 		}
 	}
+
+	Console.writeln(U"Completed save emojis");
+
+	JSON skinJson{};
+	skinJson[U"map"] = getSkinMap(skinColors);
+	skinJson[U"skins"] = skinJsonArray;
+	skinJson.save(skinOutputDirectory + U"emoji_skin.json");
+
+	Console.writeln(U"Saved skin data");
 
 	System::MessageBoxOK(U"Process completed", MessageBoxStyle::Default);
 }
