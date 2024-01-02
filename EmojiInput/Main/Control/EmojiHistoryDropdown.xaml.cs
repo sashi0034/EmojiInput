2a8f3f2c88df02d937a59cc419f4a97762f4acc2
sashi0034 <satoshi3104.nishi@gmail.com>
2024-01-02 14:21:54 +0900

    経歴ドロップダウン仮実装

---
+++ EmojiInput/Main/Control/EmojiHistoryDropdown.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EmojiInput.Main.Control;
+
+public partial class EmojiHistoryDropdown : UserControl
+{
+    public EmojiHistoryDropdown()
+    {
+        InitializeComponent();
+    }
+
+    private void button_Click(object sender, RoutedEventArgs e)
+    {
+        // throw new System.NotImplementedException();
+    }
+}
