470f32f8b6a1657ffb4b13e819b25ac295a8110e
sashi0034 <satoshi3104.nishi@gmail.com>
2024-01-02 21:57:59 +0900

    キーマップウィンドウ仮追加 (めんどくさいから作らないかも 🤪)

---
+++ EmojiInput/Keymap/KeymapWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace EmojiInput.Keymap;
+
+public partial class KeymapWindow : Window
+{
+    public KeymapWindow()
+    {
+        InitializeComponent();
+    }
+}
