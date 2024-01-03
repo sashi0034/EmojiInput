212d4839f06548b4183e8448d30927581073fab4
sashi0034 <satoshi3104.nishi@gmail.com>
2024-01-03 09:47:54 +0900

    検索機能改善 🍃

--- EmojiInput_Model/EmojiFilteredModel.cs
+++ EmojiInput_Model/EmojiFilteredModel.cs
@@ -10,17 +10,24 @@ public class EmojiFilteredModel
     private List<EmojiData> _list = new();
     public IReadOnlyList<EmojiData> List => _list;
 
+    private string _lastSearched = "";
+
     public void FilterRefresh(EmojiDatabase database, string searchText)
     {
-        var filtered = database
+        var source = searchText.Contains(_lastSearched) ? _list : database;
+        _lastSearched = searchText;
+
+        var filtered = source
             .Where(emoji =>
                 emoji.Description.Contains(searchText) || emoji.Aliases.Any(a => a.Contains(searchText)))
             .ToList();
+
         _list = filtered;
     }
 
     public void Refresh(List<EmojiData> data)
     {
+        _lastSearched = "";
         _list = data;
     }
 }
